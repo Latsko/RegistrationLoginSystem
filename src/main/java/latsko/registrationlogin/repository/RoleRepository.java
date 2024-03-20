f50d64c7867ae293544770f03769777e46afef5d
Stanislav Latsko <01latsko10@gmail.com>
2024-03-20 18:46:22 +0100

    commit #1 - basic JPA setup with User and Role entities

---
+++ src/main/java/latsko/registrationlogin/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package latsko.registrationlogin.repository;
+
+import latsko.registrationlogin.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Role findByName(String name);
+
+}
---
+++ src/main/java/latsko/registrationlogin/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package latsko.registrationlogin.repository;
+
+import latsko.registrationlogin.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByEmail(String email);
+
+}
