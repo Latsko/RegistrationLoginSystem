c6ec0e6c4c8927e633ca9e37f2f731bba2b80242
Stanislav Latsko <01latsko10@gmail.com>
2024-03-20 19:44:25 +0100

    commit #2 - register form added

---
+++ src/main/java/latsko/registrationlogin/controller/AuthController.java
@@ -0,0 +1,22 @@
+package latsko.registrationlogin.controller;
+
+import latsko.registrationlogin.dto.UserDto;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthController {
+
+    @GetMapping("/index")
+    public String home() {
+        return "index";
+    }
+
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model) {
+        UserDto userDto = new UserDto();
+        model.addAttribute("user", userDto);
+        return "register";
+    }
+}
