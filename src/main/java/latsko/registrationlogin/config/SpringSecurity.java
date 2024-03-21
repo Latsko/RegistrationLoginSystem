57244dc4bcfd59f021379c92d70936fb9458a714
Stanislav Latsko <01latsko10@gmail.com>
2024-03-21 12:41:38 +0100

    commit #7 - login and logout frond-end added, basic securityChain configured

---
+++ src/main/java/latsko/registrationlogin/config/SpringSecurity.java
@@ -0,0 +1,31 @@
+package latsko.registrationlogin.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurity {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                                .anyRequest()
+                                .authenticated())
+                .formLogin(login -> login
+                        .loginPage("/login")
+                        .loginProcessingUrl("/login")
+                        .defaultSuccessUrl("/users")
+                        .permitAll())
+                .logout(logout -> logout
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                        .permitAll());
+        return http.build();
+    }
+}
--- src/main/java/latsko/registrationlogin/controller/AuthController.java
+++ src/main/java/latsko/registrationlogin/controller/AuthController.java
@@ -25,6 +25,11 @@ public class AuthController {
         return "index";
     }
 
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
     @GetMapping("/register")
     public String showRegistrationForm(Model model) {
         UserDto userDto = new UserDto();
